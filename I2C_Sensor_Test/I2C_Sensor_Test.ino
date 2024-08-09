2b23f570546ecb168ba21e4b05d1fafbb09950e4
Mathias Gartner <mathiasgartner@gmx.at>
2024-08-09 17:42:11 +0200

    add i2c sensor tests with multiplexer board

---
+++ I2C_Sensor_Test/I2C_Sensor_Test.ino
@@ -0,0 +1,72 @@
+// see e.g. https://learn.adafruit.com/adafruit-tca9548a-1-to-8-i2c-multiplexer-breakout/arduino-wiring-and-test
+
+#include "Wire.h"
+#include "MAX30105.h"
+
+//I2C Multiplexer address
+#define TCA_ADDR 0x70
+//I2C Sensor address
+#define SENSOR_ADDR 0x57
+
+#define NUM_SENSORS 2
+#define CHNL_SENSOR_1 0
+#define CHNL_SENSOR_2 1
+const int CHNL_SENSOR[NUM_SENSORS] = { CHNL_SENSOR_1, CHNL_SENSOR_2 };
+
+MAX30105 particleSensors[NUM_SENSORS];
+
+void tcaselect(uint8_t i) {
+  if (i > 7) return;
+ 
+  Wire.beginTransmission(TCA_ADDR);
+  Wire.write(1 << i);
+  Wire.endTransmission();  
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("\n");
+
+  Wire.begin();
+
+  Serial.println("check for sensors:");
+
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    tcaselect(CHNL_SENSOR[i]);
+    Serial.print("TCA Port #");
+    Serial.println(i);
+    Wire.beginTransmission(SENSOR_ADDR);
+    if (!Wire.endTransmission()) {
+      Serial.print("Found I2C 0x");
+      Serial.println(SENSOR_ADDR, HEX);
+      
+      if (!particleSensors[i].begin())
+      {
+        Serial.println("Sensor was not found.");
+        continue;
+      }
+      particleSensors[i].setup();
+      Serial.println("setup done");
+    }
+  }
+
+}
+
+void loop() {  
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    tcaselect(CHNL_SENSOR[i]);
+    Serial.print("#");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.print(" R[");
+    Serial.print(particleSensors[i].getRed());
+    Serial.print("] IR[");
+    Serial.print(particleSensors[i].getIR());
+    Serial.print("] G[");
+    Serial.print(particleSensors[i].getGreen());
+    Serial.print("]");
+    Serial.println();
+  }
+  Serial.println();
+  delay(500);
+}
--- LED_Test/LED_Test.ino
+++ LED_Test/LED_Test.ino
@@ -1,6 +1,7 @@
 #include <FastLED.h>
 
-#define NUM_LEDS 5
+#define NUM_LEDS 70
+#define PIN_LED_DATA 9
 
 CRGB leds[NUM_LEDS];
 
@@ -9,18 +10,17 @@ int led_id;
 void setup() { 
   led_id = 0;
   Serial.begin(115200);
-  FastLED.addLeds<NEOPIXEL, 9>(leds, NUM_LEDS); 
+  FastLED.addLeds<NEOPIXEL, PIN_LED_DATA>(leds, NUM_LEDS); 
 }
 
 void loop() {
-
-	leds[led_id] = CRGB::White; 
-  FastLED.show(); 
-  delay(300);
-  Serial.println("test");
 	leds[led_id] = CRGB::Red; 
   FastLED.show(); 
-  delay(300);
+  delay(100);
+  Serial.println("test");
+	leds[led_id] = CRGB::White; 
+  FastLED.show(); 
+  delay(100);
 
   led_id += 1;
   led_id = led_id % NUM_LEDS;
